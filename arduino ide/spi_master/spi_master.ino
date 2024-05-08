fccafc52a511067274c09917f0a274a57699e8ad
“[Vishnuprasadc369]” <vishnuprasadc433@gmail.com>
2024-05-08 09:48:32 +0530

    first commit

---
+++ arduino ide/spi_master/spi_master.ino
@@ -0,0 +1,32 @@
+// the sensor communicates using SPI, so include the library:
+#include <SPI.h>
+
+void setup() {
+ 
+  pinMode(10,OUTPUT);//slave select channel select chip select
+  pinMode(11,OUTPUT);//slave select channel select chip select
+  digitalWrite(10,1);
+  Serial.begin(9600);
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
+ 
+}
+
+void loop()
+{
+  
+ int num=48;// 0 ASCCI
+  char a;
+ delay(100);
+ digitalWrite(10,0); // SLAVE SELECTION
+ for(int i=0;i<10;i++)
+ {
+  a=SPI.transfer(num++);
+  
+  Serial.write(a);
+
+  delay(100);
+ }
+   digitalWrite(10,1); // SLAVE RELEASE
+   
+ }
